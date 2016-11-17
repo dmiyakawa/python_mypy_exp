eb72c38cf21b744d733d98db2a290e8fcba26840
Daisuke Miyakawa <d.miyakawa+mac@gmail.com>
2016-11-17 09:58:00 +0900

    Initial Commit

---
+++ exp1.py
@@ -0,0 +1,13 @@
+def add1(a, b):
+    return a + b
+
+def add2(a: int, b: int) -> int:
+    return a + b
+
+def add3(a: str, b: str) -> int:
+    return a + b
+
+
+print(add1(1, 2))
+print(add2(1, 2))
+print(add3(1, 2))
